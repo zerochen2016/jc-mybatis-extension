6c8b3f0aaf25c45fca1e513aeee71d03b1079f5d
zerochen2016 <470133773@qq.com>
2019-11-19 17:47:43 +0800

    test ok

--- src/main/java/jc/mybatis/extension/generate/ServiceGenerator.java
+++ src/main/java/jc/mybatis/extension/generate/ServiceGenerator.java
@@ -17,18 +17,27 @@ import jc.mybatis.extension.util.StringUtil;
 
 public class ServiceGenerator {
 
-	public static void generate(String tableName, String projectPath, String servicePackage, String mapperPackage,
-			String entityPackage, String entityExamplePackage, String url, String user, String password, String driverClassName) 
+	public static void generate(String tableName) 
 					throws IOException {
+		
+		String projectPath = new File("").getAbsolutePath();
+		File mybatisYml = new File(new StringBuffer(projectPath).append("/src/main/resources/mybatis/mybatis-extension.yml").toString());
+		Properties prop = new Properties();
+		InputStream in = new FileInputStream(mybatisYml);
+		prop.load(in);
+		String url = prop.getProperty("url");
+		String user = prop.getProperty("user");
+		String password = prop.getProperty("password");
+		String driverClassName = prop.getProperty("driverClassName");
+		String servicePackage = prop.getProperty("servicePackage");
+		String entityPackage = prop.getProperty("entityPackage");
+		String entityExamplePackage = prop.getProperty("entityExamplePackage");
+		String mapperPackage = prop.getProperty("mapperPackage");
 		
 		String servicePackagePath = servicePackage.replace(".", "/");
 		String servicePath = new StringBuffer(projectPath).append("/src/main/java/")
 				.append(servicePackagePath).append("/").toString();
 		String serviceImplPath = new StringBuffer(servicePath).append("impl/").toString();
-		System.out.println("----------------------------------------");
-		System.out.println("servicePackagePath = " + servicePackagePath);
-		System.out.println("servicePath = " + servicePath);
-		System.out.println("serviceImplPath = " + serviceImplPath);
 		File serviceDir = new File(serviceImplPath);
 		if (!serviceDir.exists()) {
 			serviceDir.mkdirs();
@@ -269,5 +278,4 @@ public class ServiceGenerator {
 		serviceLines.add("}");
 		return serviceLines;
 	}
-
 }
